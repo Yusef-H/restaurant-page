a66e414f9533e2ca9b65d5e494221beaf0426762
Joseph <yusefhalabi456@gmail.com>
2022-01-01 14:38:56 +0200

    Setup all webpack configurations and added background image

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Icon from './background.jpg';
+
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = "oooooo";
+    element.classList.add('hello');
+
+    return element;
+}
+
+
+
